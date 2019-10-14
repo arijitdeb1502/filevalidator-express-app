0bb32606770914cb341f5f49159065e33f0726fc
arijitdeb1502 <arijitdeb.git@gmail.com>
2019-10-15 07:30:53 +0800

    Pushing for heroku

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const port=process.env.PORT||3000;
 //Sample Request String:  http://localhost:3000/fixedfilemap?fld=inpfld2&key=inpFld5&keyval=689208
 //https://file-validator-filesystem.herokuapp.com/fixedfilemap?fld=inpfld2&key=inpFld5&keyval=689208
 
+// D:\workspace_NJ_ANG_EXT\file-validator-app>heroku create ardeb-file-validator-app
+// Creating ⬢ ardeb-file-validator-app... done
+// https://ardeb-file-validator-app.herokuapp.com/ | https://git.heroku.com/ardeb-file-validator-app.git
 
+// D:\workspace_NJ_ANG_EXT\file-validator-app>
 
 
 app.use(express.json());
